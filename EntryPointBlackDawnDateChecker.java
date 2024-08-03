a06ea759d291c43c5c0c9d213dfcb1f0bb1d3ad9
Tim <50602271+Tim123467@users.noreply.github.com>
2024-08-03 04:42:48 -0400

    Update EntryPointBlackDawnDateChecker.java

--- EntryPointBlackDawnDateChecker.java
+++ EntryPointBlackDawnDateChecker.java
@@ -43,8 +43,10 @@ public class EntryPointBlackDawnDateChecker {
 					System.out.println("Date: " + line.substring(37, 47));
 					System.out.println("Time: " + line.substring(48, line.length() - 3));
 				}
-			} catch (Exception e) {
+			} catch (IOException e) {
 				System.out.println("This user does not exist.");
+				continue;
+			} catch (Exception e) {
 				continue;
 			}
 		}
