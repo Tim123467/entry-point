521b961d6fbe1b0e258a1a0815092597f8443597
Tim <50602271+Tim123467@users.noreply.github.com>
2024-09-21 23:34:17 -0400

    Update EntryPointProbabilities.java

--- EntryPointProbabilities.java
+++ EntryPointProbabilities.java
@@ -45,7 +45,7 @@ public class EntryPointProbabilities {
 	
 	public static void main(String[] args) {
 		try (BufferedReader readFile = new BufferedReader(new FileReader("EP Daily Challenges Wikitext.txt"));) {
-			//The Short values will represent the number of occurrences
+			//the Short values will represent the number of occurrences
 			Map<Mission, Map<Modifier, Short>> stealth2021 = new EnumMap<>(getMissionClass());
 			Map<Mission, Map<Modifier, Short>> loud2021 = new EnumMap<>(getMissionClass());
 			Map<Mission, Map<Modifier, Short>> stealth2022 = new EnumMap<>(getMissionClass());
