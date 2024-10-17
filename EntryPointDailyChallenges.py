0ba4775d74d84ac8812a2c1f83fd3a498a49867e
Tim <50602271+Tim123467@users.noreply.github.com>
2024-10-17 01:17:03 -0400

    Update EntryPointDailyChallenges.py
    Backup version in case the change I'm about to make goes wrong.

--- EntryPointDailyChallenges.py
+++ EntryPointDailyChallenges.py
@@ -1,156 +1,135 @@
-# -*- coding: utf-8 -*-
 '''
 Program used to translate Breakbar's txt file into wikitext. Note that I renamed the txt file to "EP Daily Challenges.txt"
 The only edits that should be made are to the values of the variables near the top: txtFileName, currentYearAndMonth, and lastYear.
 (unless you want to try to improve the code)
-The output is produced in a separate text file.
+The output is produced in the console (which should allow enough lines of output to be displayed, confirmed on Google Colab).
 '''
+import urllib
 
-txtFileName = 'EP Daily Challenges.txt' #change depending on the name you set for Breakbar's txt file
-currentYearAndMonth = '2024-09' #must be in the format "YYYY-MM"
-lastYear = 2025 #the last year that the txt file has daily challenges for, as an integer
+fileName = 'EP Daily Challenges.txt' #change depending on the name you want for Breakbar's txt file
 
-while True: #prompts for year
-    try:
-        year = input('Enter the year you want the wikitext for: ')
-        if 2022 <= int(year) <= lastYear:
+urllib.request.urlretrieve('https://github.com/The-breakbar/Entry-Point-Wiki-Bot/raw/main/utils/DailyChallengeList.txt', fileName)
+
+currentYearAndMonth = '2024-10' #must be in the format "YYYY-MM"
+year = '2024' #change manually for the year you want the wikitext for
+#currently, the txt file covers the end of 2022 to most of 2025
+
+with open(fileName) as fin:
+    while True:
+        line = fin.readline()
+        if line == '': #stops the loop when there's no more lines to read
             break
-        else:
-            print(f'The txt file only has daily challenges for years starting at 2022 and ending at {lastYear}.')
-    except ValueError:
-        print('Invalid input.')
-
-with open(txtFileName) as fin:
-    with open('EP Daily Challenges Generated Wikitext.txt', 'w') as fout:
-        while True:
-            line = fin.readline()
-            if line == '': #stops the loop when there's no more lines to read
-                break
-            date = line[:line.index(',')]
+        date = line[:line.index(',')]
             
-            if date[:4] == year: #checks if the year matches the specified year
-                line = line[line.index(',') + 1:]
-                mission = line[:line.index(',')]
-                line = line[line.index(',') + 1:]
-                tactic = line[:line.index(',')]
-                line = line[line.index(',') + 1:]
-                color1 = line[:line.index(',')]
-                line = line[line.index(',') + 1:]
-                mod1 = line[:line.index(',')]
-                mod1 = 'Takedown Limit (4)' if mod1 == 'Takedown Limit' else 'Takedown Limit (6)' if mod1 == 'Takedown Limit 6' else mod1
-                line = line[line.index(',') + 1:]
-                color2 = line[:line.index(',')]
-                line = line[line.index(',') + 1:]
-                mod2 = line[:line.index(',')]
-                mod2 = 'Takedown Limit (4)' if mod2 == 'Takedown Limit' else 'Takedown Limit (6)' if mod2 == 'Takedown Limit 6' else mod2
-                line = line[line.index(',') + 1:]
-                color3 = line[:line.index(',')]
-                mod3 = line[line.index(',') + 1:].strip()
-                mod3 = 'Takedown Limit (4)' if mod3 == 'Takedown Limit' else 'Takedown Limit (6)' if mod3 == 'Takedown Limit 6' else mod3
+        if date[:4] == year: #checks if the year matches the specified year
+            line = line[line.index(',') + 1:]
+            mission = line[:line.index(',')]
+            line = line[line.index(',') + 1:]
+            tactic = line[:line.index(',')]
+            line = line[line.index(',') + 1:]
+            color1 = line[:line.index(',')]
+            line = line[line.index(',') + 1:]
+            mod1 = line[:line.index(',')]
+            mod1 = 'Takedown Limit (4)' if mod1 == 'Takedown Limit' else 'Takedown Limit (6)' if mod1 == 'Takedown Limit 6' else mod1
+            line = line[line.index(',') + 1:]
+            color2 = line[:line.index(',')]
+            line = line[line.index(',') + 1:]
+            mod2 = line[:line.index(',')]
+            mod2 = 'Takedown Limit (4)' if mod2 == 'Takedown Limit' else 'Takedown Limit (6)' if mod2 == 'Takedown Limit 6' else mod2
+            line = line[line.index(',') + 1:]
+            color3 = line[:line.index(',')]
+            mod3 = line[line.index(',') + 1:].strip()
+            mod3 = 'Takedown Limit (4)' if mod3 == 'Takedown Limit' else 'Takedown Limit (6)' if mod3 == 'Takedown Limit 6' else mod3
                 
-                if date[8:10] == '01': #first day of the month
-                    if date[5:7] == '01': #first month of the year
-                        fout.write(f"'''Daily Challenges''' from {year} will be displayed here.\n\n===January")
-                    else:
-                        fout.write('|}\n===')
-                        if date[5:7] == '02':
-                            fout.write('February')
-                        elif date[5:7] == '03':
-                            fout.write('March')
-                        elif date[5:7] == '04':
-                            fout.write('April')
-                        elif date[5:7] == '05':
-                            fout.write('May')
-                        elif date[5:7] == '06':
-                            fout.write('June')
-                        elif date[5:7] == '07':
-                            fout.write('July')
-                        elif date[5:7] == '08':
-                            fout.write('August')
-                        elif date[5:7] == '09':
-                            fout.write('September')
-                        elif date[5:7] == '10':
-                            fout.write('October')
-                        elif date[5:7] == '11':
-                            fout.write('November')
-                        elif date[5:7] == '12':
-                            fout.write('December')
-                        else:
-                            raise ValueError('The part in the txt file corresponding with the month was invalid.')
-                    fout.write(f' {year}===\n{{| class="fandom-table article-table mw-collapsible mw-collapsed" ')
-                    
-                    if date[:7] == currentYearAndMonth: #current year and current month
-                        fout.write('data-expandtext="Show Previous and Upcoming Challenges" data-collapsetext="Hide Previous and Upcoming Challenges"\n')
-                    elif int(year) < int(currentYearAndMonth[:4]) or (int(year) == int(currentYearAndMonth[:4]) and int(date[5:7]) < int(currentYearAndMonth[5:7])):
-                        #earlier year OR same year but earlier month
-                        fout.write('data-expandtext="Show Previous Challenges" data-collapsetext="Hide Previous Challenges"\n')
-                    else: #current year and later month OR later year
-                        fout.write('data-expandtext="Show Upcoming Challenges" data-collapsetext="Hide Upcoming Challenges"\n')
-                    fout.write('!Date\n!Mission\n!Tactic\n!Modifiers\n')
-                
-                fout.write('|-\n!' + date + '\n|')
-                
-                if mission == 'The Scientist' and mod1 == 'No Suppressors' and mod2 == 'Hidden UI' and mod3 == 'No Knockouts': #invalid modifier combinations
-                    fout.write('The Scientist\n|Stealth\n|<span class=challenge-green>Fog</span>, <span class=challenge-green>No Suppressors</span>, <span class=challenge-purple>Hidden UI</span>\n')
-                elif mission == 'The Scientist' and (mod1 == 'No Suppressors' or mod2 == 'No Suppressors') and mod3 == 'No Knockouts':
-                    fout.write(f'The Scientist\n|Stealth\n|<span class=challenge-green>{mod1}</span>, <span class=challenge-')
-                    
-                    if color2 == 'G':
-                        fout.write('green')
-                    elif color2 == 'B':
-                        fout.write('blue')
-                    elif color2 == 'P':
-                        fout.write('purple')
-                    elif color2 == 'R':
-                        fout.write('red')
-                    else:
-                        raise ValueError("The part in the txt file corresponding with the second modifier's color was invalid.")
-                    
-                    fout.write(f'>{mod2}</span>, <span class=challenge-purple>Hidden UI</span>\n')
+            if date[8:10] == '01': #first day of the month
+                if date[5:7] == '01': #first month of the year
+                    print(f"'''Daily Challenges''' from {year} will be displayed here.\n\n===January",end='')
                 else:
-                    if mission == 'The Auction' or mission == 'The Gala' or mission == 'The Cache' or mission == 'The Setup' or mission == 'The Lockup' or mission == 'The Score':
-                        fout.write('{{Robux}} ')
-                    
-                    fout.write(f'{mission}\n|{tactic}\n|<span class=challenge-')
-                    
-                    if color1 == 'G':
-                        fout.write('green')
-                    elif color1 == 'B':
-                        fout.write('blue')
-                    elif color1 == 'P':
-                        fout.write('purple')
-                    elif color1 == 'R':
-                        fout.write('red')
+                    print('|}\n===',end='')
+                    if date[5:7] == '02':
+                        print('February',end='')
+                    elif date[5:7] == '03':
+                        print('March',end='')
+                    elif date[5:7] == '04':
+                        print('April',end='')
+                    elif date[5:7] == '05':
+                        print('May',end='')
+                    elif date[5:7] == '06':
+                        print('June',end='')
+                    elif date[5:7] == '07':
+                        print('July',end='')
+                    elif date[5:7] == '08':
+                        print('August',end='')
+                    elif date[5:7] == '09':
+                        print('September',end='')
+                    elif date[5:7] == '10':
+                        print('October',end='')
+                    elif date[5:7] == '11':
+                        print('November',end='')
+                    elif date[5:7] == '12':
+                        print('December',end='')
                     else:
-                        raise ValueError("The part in the txt file corresponding with the first modifier's color was invalid.")
+                        raise ValueError('The part in the txt file corresponding with the month was invalid.')
+                print(f' {year}===\n{{| class="fandom-table article-table mw-collapsible mw-collapsed" ',end='')
                     
-                    fout.write(f'>{mod1}</span>, <span class=challenge-')
+                if date[:7] == currentYearAndMonth: #current year and current month
+                    print('data-expandtext="Show Previous and Upcoming Challenges" data-collapsetext="Hide Previous and Upcoming Challenges"')
+                elif int(year) < int(currentYearAndMonth[:4]) or (int(year) == int(currentYearAndMonth[:4]) and int(date[5:7]) < int(currentYearAndMonth[5:7])):
+                    #earlier year OR same year but earlier month
+                    print('data-expandtext="Show Previous Challenges" data-collapsetext="Hide Previous Challenges"')
+                else: #current year and later month OR later year
+                    print('data-expandtext="Show Upcoming Challenges" data-collapsetext="Hide Upcoming Challenges"')
+                print('!Date\n!Mission\n!Tactic\n!Modifiers')
+                
+            print('|-\n!' + date + '\n|',end='')
+                
+            if mission == 'The Scientist' and mod1 == 'No Suppressors' and mod2 == 'Hidden UI' and mod3 == 'No Knockouts': #invalid modifier combinations
+                print('The Scientist\n|Stealth\n|<span class=challenge-green>Fog</span>, <span class=challenge-green>No Suppressors</span>, <span class=challenge-purple>Hidden UI</span>')
+            elif mission == 'The Scientist' and (mod1 == 'No Suppressors' or mod2 == 'No Suppressors') and mod3 == 'No Knockouts':
+                if color2 == 'G':
+                    print(f'The Scientist\n|Stealth\n|<span class=challenge-green>{mod1}</span>, <span class=challenge-green>{mod2}</span>, <span class=challenge-purple>Hidden UI</span>')
+                elif color2 == 'B':
+                    print(f'The Scientist\n|Stealth\n|<span class=challenge-green>{mod1}</span>, <span class=challenge-blue>{mod2}</span>, <span class=challenge-purple>Hidden UI</span>')
+                elif color2 == 'P':
+                    print(f'The Scientist\n|Stealth\n|<span class=challenge-green>{mod1}</span>, <span class=challenge-purple>{mod2}</span>, <span class=challenge-purple>Hidden UI</span>')
+                else:
+                    raise ValueError("The part in the txt file corresponding with the second modifier's color was invalid.")
+            else:
+                if mission == 'The Auction' or mission == 'The Gala' or mission == 'The Cache' or mission == 'The Setup' or mission == 'The Lockup' or mission == 'The Score':
+                    print('{{Robux}} ',end='')
                     
-                    if color2 == 'G':
-                        fout.write('green')
-                    elif color2 == 'B':
-                        fout.write('blue')
-                    elif color2 == 'P':
-                        fout.write('purple')
-                    elif color2 == 'R':
-                        fout.write('red')
-                    else:
-                        raise ValueError("The part in the txt file corresponding with the second modifier's color was invalid.")
+                if color1 == 'G':
+                    print(f'{mission}\n|{tactic}\n|<span class=challenge-green>{mod1}</span>, ',end='')
+                elif color1 == 'B':
+                    print(f'{mission}\n|{tactic}\n|<span class=challenge-blue>{mod1}</span>, ',end='')
+                elif color1 == 'P':
+                    print(f'{mission}\n|{tactic}\n|<span class=challenge-purple>{mod1}</span>, ',end='')
+                elif color1 == 'R':
+                    print(f'{mission}\n|{tactic}\n|<span class=challenge-red>{mod1}</span>, ',end='')
+                else:
+                    raise ValueError("The part in the txt file corresponding with the first modifier's color was invalid.")
                     
-                    fout.write(f'>{mod2}</span>, <span class=challenge-')
+                print(f'',end='')
                     
-                    if color3 == 'G':
-                        fout.write('green')
-                    elif color3 == 'B':
-                        fout.write('blue')
-                    elif color3 == 'P':
-                        fout.write('purple')
-                    elif color3 == 'R':
-                        fout.write('red')
-                    else:
-                        raise ValueError("The part in the txt file corresponding with the third modifier's color was invalid.")
+                if color2 == 'G':
+                    print(f'<span class=challenge-green>{mod2}</span>, ',end='')
+                elif color2 == 'B':
+                    print(f'<span class=challenge-blue>{mod2}</span>, ',end='')
+                elif color2 == 'P':
+                    print(f'<span class=challenge-purple>{mod2}</span>, ',end='')
+                elif color2 == 'R':
+                    print(f'<span class=challenge-red>{mod2}</span>, ',end='')
+                else:
+                    raise ValueError("The part in the txt file corresponding with the second modifier's color was invalid.")
                     
-                    fout.write(f'>{mod3}</span>\n')
-        
-        fout.write('|}')
-        print('Script has been executed.')
+                if color3 == 'G':
+                    print(f'<span class=challenge-green>{mod3}</span>')
+                elif color3 == 'B':
+                    print(f'<span class=challenge-blue>{mod3}</span>')
+                elif color3 == 'P':
+                    print(f'<span class=challenge-purple>{mod3}</span>')
+                elif color3 == 'R':
+                    print(f'<span class=challenge-red>{mod3}</span>')
+                else:
+                    raise ValueError("The part in the txt file corresponding with the third modifier's color was invalid.")
+    print('|}')
